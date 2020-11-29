48cf1f1eb871966565cfa5ea541c62e6d43c241a
ssbmars <46971717+ssbmars@users.noreply.github.com>
2020-11-28 20:17:45 -0800

    create .release folder in root, update relevant directories

---
+++ .release/singleplayer version/bn3plus patch/README-bn3plus.txt
@@ -0,0 +1,9 @@
+
+This patch is entirely optional. 
+Note that it will cause certain menus and animations to take longer to load.
+
+This is a modified version of Prof9's BN3Plus patch, which was not natively compatible with BBN3.
+
+This patch contains the extra Navi Customizer and Jack In animations from the EXE version.
+
+Apply this patch over a rom that has already had BBN3_pve.bps applied.
--- generate patches.bat
+++ generate patches.bat
@@ -7,22 +7,22 @@ textpet run-script pve_eng.tpl
 
 echo      creating pvp patch
 
-flips -c -b "rom/bn3blue.gba" "bbn3.gba" ".ignore/release/bbn3.bps"
+flips -c -b "rom/bn3blue.gba" "bbn3.gba" ".release/bbn3.bps"
 
 echo      creating singleplayer patch
 
-flips -c -b "rom/bn3blue.gba" "bbn3_pve.gba" ".ignore/release/singleplayer version/bbn3_pve.bps"
+flips -c -b "rom/bn3blue.gba" "bbn3_pve.gba" ".release/singleplayer version/bbn3_pve.bps"
 
 echo      creating randomizer patch
 
-flips -c -b "rom/bn3blue.gba" "rom/output.gba" ".ignore/release/singleplayer version/bbn3_pve_randomize.bps"
+flips -c -b "rom/bn3blue.gba" "rom/output.gba" ".release/singleplayer version/bbn3_pve_randomize.bps"
 
 
 armips patch.asm -equ IS_PVP 0 -equ IS_ENG 1 -definelabel IS_BN3PLUS 1
 textpet run-script pve_bn3plus_eng.tpl
 
 echo      creating bbn3plus patch
-flips -c -b "bbn3_pve.gba" "bbn3_pve_bn3plus.gba" ".ignore/release/singleplayer version/bn3plus patch/bbn3_pve_bn3plus.bps"
+flips -c -b "bbn3_pve.gba" "bbn3_pve_bn3plus.gba" ".release/singleplayer version/bn3plus patch/bbn3_pve_bn3plus.bps"
 
 
 timeout 6
