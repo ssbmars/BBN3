20940fe7e6fc41d2c4d446bb9295b9e1d3affe46
ssbmars <46971717+ssbmars@users.noreply.github.com>
2020-11-28 18:53:40 -0800

    automate the creation of patch files for public release
    creates a file for 1) pvp, 2) pve, 3) pve without textpet modifications (randomizer-friendly), 4) pve with bn3plus modifications enabled (only compatible with patch #2)

---
+++ generate patches.bat
@@ -0,0 +1,28 @@
+@echo off
+armips patch.asm -equ IS_PVP 1 -equ IS_ENG 1 -sym bbn3.sym 
+textpet run-script pvp_eng.tpl
+
+armips patch.asm -equ IS_PVP 0 -equ IS_ENG 1 -sym bbn3_pve.sym
+textpet run-script pve_eng.tpl
+
+echo      creating pvp patch
+
+flips -c -b "rom/bn3blue.gba" "bbn3.gba" ".ignore/release/bbn3.bps"
+
+echo      creating singleplayer patch
+
+flips -c -b "rom/bn3blue.gba" "bbn3_pve.gba" ".ignore/release/singleplayer version/bbn3_pve.bps"
+
+echo      creating randomizer patch
+
+flips -c -b "rom/bn3blue.gba" "rom/output.gba" ".ignore/release/singleplayer version/bbn3_pve_randomize.bps"
+
+
+armips patch.asm -equ IS_PVP 0 -equ IS_ENG 1 -definelabel IS_BN3PLUS 1
+textpet run-script pve_bn3plus_eng.tpl
+
+echo      creating bbn3plus patch
+flips -c -b "bbn3_pve.gba" "bbn3_pve_bn3plus.gba" ".ignore/release/singleplayer version/bn3plus patch/bbn3_pve_bn3plus.bps"
+
+
+timeout 6
