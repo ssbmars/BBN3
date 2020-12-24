1a1dd38dd99bc1cb6840f6000e52f927969e38eb
ssbmars <46971717+ssbmars@users.noreply.github.com>
2020-12-23 23:22:49 -0800

    batch to generate a .bps file with the debugging code enabled, for testing

---
+++ generate debugging patches.bat
@@ -0,0 +1,13 @@
+@echo off
+armips patch.asm -equ IS_PVP 1 -equ IS_ENG 1 -equ AM_DEBUGGING 1 -sym bbn3.sym 
+textpet run-script pvp_eng.tpl
+
+echo      creating debugging pvp patch 
+
+flips -c -b "rom/bn3blue.gba" "bbn3.gba" ".ignore/BBN3.bps"
+
+echo      creating debugging pvp patch without textpet
+
+flips -c -b "rom/bn3blue.gba" "rom/output.gba" ".ignore/BBN3_no-tp.bps"
+
+timeout 4
