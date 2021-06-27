5c734b7e06006b6008418d1106d46b226374610b
ssbmars <46971717+ssbmars@users.noreply.github.com>
2021-06-26 20:52:20 -0700

    fix args for rollback build script

--- build_rollback.bat
+++ build_rollback.bat
@@ -1,4 +1,11 @@
 @echo off
 armips patch.asm -equ IS_PVP 1 -equ IS_ENG 1 -equ AM_DEBUGGING 1 -equ IS_ROLLBACK 1 -sym bbn3.sym 
 textpet run-script pvp_eng.tpl
+
+echo  making copy of rollback rom
+xcopy /v /y "BBN3.gba" ".rollback\rom\BBN3 Online.gba"
+
+echo      creating rollback patch
+flips -c -b "rom/bn3blue.gba" "BBN3.gba" ".rollback/BBN3_Online.bps"
+
 timeout 3
--- generate patches.bat
+++ generate patches.bat
@@ -1,13 +1,13 @@
 @echo off
 
-armips patch.asm -equ IS_PVP 1 -equ IS_ENG 1 -equ AM_DEBUGGING 0 -equ IS_ROLLBACK 0 -sym bbn3.sym 
+armips patch.asm -equ IS_PVP 1 -equ IS_ENG 1 -equ AM_DEBUGGING 0 -equ IS_ROLLBACK 1 -sym bbn3.sym 
 textpet run-script pvp_eng.tpl
 
 echo  making copy of rollback rom
-xcopy /v /y "BBN3.gba" ".rollback\rom\"
+xcopy /v /y /i "BBN3.gba" ".rollback\rom\BBN3 Online.gba"
 
 echo      creating rollback patch
-flips -c -b "rom/bn3blue.gba" "BBN3.gba" ".rollback/BBN3.bps"
+flips -c -b "rom/bn3blue.gba" "BBN3.gba" ".rollback/BBN3_Online.bps"
 
 
 armips patch.asm -equ IS_PVP 1 -equ IS_ENG 1 -equ AM_DEBUGGING 0 -equ IS_ROLLBACK 0 -sym bbn3.sym 
