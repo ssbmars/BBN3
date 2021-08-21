dd4a89ddaf4aab08fc399e1f36889329475c2111
ssbmars <46971717+ssbmars@users.noreply.github.com>
2021-08-21 01:45:00 -0700

    add spanish patches to the release build script

--- generate patches.bat
+++ generate patches.bat
@@ -9,7 +9,8 @@ xcopy /v /y /i "BBN3.gba" ".rollback\rom\BBN3 Online.gba"
 
 echo      creating rollback patch
 flips -c -b "rom/bn3blue.gba" "BBN3.gba" ".rollback/BBN3_Online.bps"
-
+echo      creating Spanish rollback patch
+flips -c -b "rom/bn3blue.gba" "BBN3_esp.gba" ".rollback/BBN3_Online_Spanish.bps"
 
 armips patch.asm -equ IS_PVP 1 -equ IS_ENG 1 -equ AM_DEBUGGING 0 -equ IS_ROLLBACK 0 -sym bbn3.sym 
 textpet run-script pvp_eng.tpl
@@ -17,25 +18,31 @@ textpet run-script pvp_esp.tpl
 
 armips patch.asm -equ IS_PVP 0 -equ IS_ENG 1 -equ AM_DEBUGGING 0 -equ IS_ROLLBACK 0 -sym bbn3_pve.sym
 textpet run-script pve_eng.tpl
+textpet run-script pve_esp.tpl
 
 echo      creating pvp patch
-
 flips -c -b "rom/bn3blue.gba" "BBN3.gba" ".release/BBN3.bps"
 
 echo      creating singleplayer patch
-
-flips -c -b "rom/bn3blue.gba" "bbn3_pve.gba" ".release/singleplayer version/BBN3_pve.bps"
+flips -c -b "rom/bn3blue.gba" "BBN3_pve.gba" ".release/singleplayer version/BBN3_pve.bps"
 
 echo      creating randomizer patch
-
 flips -c -b "rom/bn3blue.gba" "rom/output.gba" ".release/singleplayer version/BBN3_pve_randomize.bps"
+
+echo      creating Spanish pvp patch
+flips -c -b "rom/bn3blue.gba" "BBN3_esp.gba" ".release/Spanish/BBN3_Spanish.bps"
+
+echo      creating Spanish singleplayer patch
+flips -c -b "rom/bn3blue.gba" "BBN3_pve_esp.gba" ".release/Spanish/singleplayer version/BBN3_pve_Spanish.bps"
 
 
 armips patch.asm -equ IS_PVP 0 -equ IS_ENG 1 -definelabel IS_BN3PLUS 1 -equ AM_DEBUGGING 0 -equ IS_ROLLBACK 0
 textpet run-script pve_bn3plus_eng.tpl
+textpet run-script pve_bn3plus_esp.tpl
 
 echo      creating bbn3plus patch
-flips -c -b "bbn3_pve.gba" "bbn3_pve_bn3plus.gba" ".release/singleplayer version/bn3plus patch/BBN3_pve_bn3plus.bps"
-
+flips -c -b "BBN3_pve.gba" "BBN3_pve_bn3plus.gba" ".release/singleplayer version/bn3plus patch/BBN3_pve_bn3plus.bps"
+echo      creating Spanish bbn3plus patch
+flips -c -b "BBN3_pve_esp.gba" "BBN3_pve_bn3plus_esp.gba" ".release/Spanish/singleplayer version/bn3plus patch/BBN3_pve_bn3plus_Spanish.bps"
 
 timeout 6
---
+++ pve_bn3plus_esp.tpl
@@ -0,0 +1,16 @@
+load-plugins "tp\plugins"
+game mmbn3
+load-file-index "tp\indexes\mmbn3b-us.tpi"
+read-text-archives "rom\output.gba" 
+
+trim --preserve-commands --split-text --max 2
+trim --preserve-commands --max 1
+read-text-archives "tp\scripts\spanish\" --patch --format tpl
+
+read-text-archives "tp\scripts\spanish2\chip_text_esp.tpl" -p
+read-text-archives "tp\scripts\spanish2\comm_menu_esp.tpl" -p
+read-text-archives "tp\scripts\spanish2\misc_esp.tpl" -p
+read-text-archives "tp\scripts\spanish2\ncp_text_esp.tpl" -p
+
+
+write-text-archives "BBN3_pve_bn3plus_esp.gba"
